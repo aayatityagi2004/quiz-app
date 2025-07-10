7332b1219470603c0d01d1a05fe6cb703e18b748
Aayati Tyagi <aayatityagi200427@gmail.com>
2025-07-10 15:05:16 +0530

    initial commit with quiz files

---
+++ quiz.js
@@ -0,0 +1,55 @@
+const URL = "https://opentdb.com/api.php?amount=5&category=19&difficulty=easy&type=multiple";
+
+const questions = document.querySelector(".question-container");
+const choices = document.querySelector("#option-buttons");
+const next = document.querySelector(".NEXT");
+let counter=0;
+
+let currentindex = 0;
+let quizzes = [];
+async function getdata (){
+    let response = await fetch(URL);
+    let data = await response.json();
+    quizzes = data.results;
+    printData();
+    getChoices();
+    console.log(quizzes);
+}
+
+function printData(){
+   questions.innerHTML = quizzes[currentindex].question;
+};
+
+ next.addEventListener('click',() => {
+    currentindex ++;
+    printData();
+    getChoices();
+    }
+)
+
+function getChoices(){
+    let allAnswers = [...quizzes[currentindex].incorrect_answers, quizzes[currentindex].correct_answer];
+    allAnswers.sort(()=> Math.random() - 0.5);
+    choices.innerHTML="";
+    for(let i=0; i<allAnswers.length; i++){
+        let btn = document.createElement("button");
+        btn.innerText = allAnswers[i];
+        btn.className= "option-buttons";
+        btn.addEventListener('click', ()=> checkAnswer(allAnswers[i]));
+        choices.appendChild(btn);
+    }
+};
+
+function checkAnswer(selectedAnswer){
+    const correctAnswer = quizzes[currentindex].correct_answer;
+    if(selectedAnswer===correctAnswer){
+        alert("CORRECT ANSWER!!");
+        counter++;
+        document.getElementById("counter").innerText = counter;
+    }else {
+        alert(`WRONG! The correct answer is: ${correctAnswer}`);
+    }
+}
+
+
+getdata();
